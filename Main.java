7efe948df0fabb82180d971ebc6b7a11e0fd7330
Jadaah <jadaah.wingfield@patelhs.org>
2020-11-10 15:14:25 +0000

    i created a while loop to compare seats with names

--- Main.java
+++ Main.java
@@ -14,11 +14,12 @@ class Main {
       // enter the name
       names[i] = scan.next();
     }
-    
+
     // loop through names and seats
     int count = 0;
     while (count < 15)
     {
+      
       //print out correlating names and seats
       System.out.println(names[count] + "sits in seat #" + seats[count]);
       count++;
