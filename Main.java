4eb357be5b53fef80a872668bc1af36d53c81817
Jadaah <jadaah.wingfield@patelhs.org>
2020-11-10 14:59:37 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    int[] seats = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+    // declare the seat number
+    String[] names = new String[15];
+    // declare the unknown names
+    Scanner scan = new Scanner(System.in);
+
+    for (int i = 0; i < 15; i++) {
+      System.out.println("input name");
+      // enter the name
+      names[i] = scan.next();
+    }
+    // loop through names and seats
+    int count = 0;
+    while (count < 15)
+    {
+      //print out correlating names and seats
+      System.out.println(names[count] + "sits in seat #" + seats[count]);
+      count++;
+    }
+
+  }
+}
